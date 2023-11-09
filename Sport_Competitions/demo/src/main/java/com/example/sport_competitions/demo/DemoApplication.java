b01a8e8a4eeca52ae16fbddedb87827d1cdc9122
Maksern <maks.men9922@gmail.com>
2023-11-09 20:44:14 +0200

    Initialize the project

---
+++ Sport_Competitions/demo/src/main/java/com/example/sport_competitions/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sport_competitions.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
